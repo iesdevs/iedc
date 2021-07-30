fdd349e0a9570ecd5471e7bad2661e6b7cf00189
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-07-30 12:24:53 +0530

    ✨ feat: adds email functionality

---
+++ scripts/form.js
@@ -0,0 +1,20 @@
+var form = document.getElementById("contactForm");
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
